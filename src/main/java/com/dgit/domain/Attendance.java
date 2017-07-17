bdf70daee1d1a655d5cf0f48ecf3c6abea119453
rksym343 <rksym343@naver.com>
2017-07-17 21:03:45 +0900

    ccc

--- src/main/java/com/dgit/domain/Attendance.java
+++ src/main/java/com/dgit/domain/Attendance.java
@@ -5,56 +5,40 @@ import java.util.Date;
 public class Attendance {
 
 	private int atNo;
-	private String sId;
-	private Date inTime;
-	private Date outTime;
+	private Student student;
+	private Date theTime;
 	private AttendanceStatus attendanceStatus;
-
 	public int getAtNo() {
 		return atNo;
 	}
-
 	public void setAtNo(int atNo) {
 		this.atNo = atNo;
 	}
-
-	public String getsId() {
-		return sId;
+	
+	public Student getStudent() {
+		return student;
 	}
-
-	public void setsId(String sId) {
-		this.sId = sId;
+	public void setStudent(Student student) {
+		this.student = student;
 	}
-
-	public Date getInTime() {
-		return inTime;
+	public Date getTheTime() {
+		return theTime;
 	}
-
-	public void setInTime(Date inTime) {
-		this.inTime = inTime;
+	public void setTheTime(Date theTime) {
+		this.theTime = theTime;
 	}
-
-	public Date getOutTime() {
-		return outTime;
-	}
-
-	public void setOutTime(Date outTime) {
-		this.outTime = outTime;
-	}
-
 	public AttendanceStatus getAttendanceStatus() {
 		return attendanceStatus;
 	}
-
 	public void setAttendanceStatus(AttendanceStatus attendanceStatus) {
 		this.attendanceStatus = attendanceStatus;
 	}
-
 	@Override
 	public String toString() {
-		return String.format("Attendance [atNo=%s, sId=%s, inTime=%s, outTime=%s, attendanceStatus=%s]", atNo, sId,
-				inTime, outTime, attendanceStatus);
+		return String.format("Attendance [atNo=%s, sId=%s, theTime=%s, attendanceStatus=%s]", atNo, student, theTime,
+				attendanceStatus);
 	}
+
 	
 	
 
--- src/main/java/com/dgit/persistence/AttendanceDAO.java
+++ src/main/java/com/dgit/persistence/AttendanceDAO.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Map;
 
 import com.dgit.domain.Attendance;
+import com.dgit.domain.AttendanceSearchCriteria;
 
 public interface AttendanceDAO {
-
+	
+	public void insertAttendance(Attendance attendance) throws Exception;
 	public void insertAttendanceToday(String sId) throws Exception;
 	public void updateAttendance(Attendance attendance) throws Exception; 
-	public void cancelAttendance(String sId) throws Exception;
-	public List<Attendance> selectAttendanceByCri(Map<String, Object> param) throws Exception;
+	public void updateAttendanceToday(Attendance attendance) throws Exception; 
+	public void deleteAttendance(Attendance attendance) throws Exception;
+	public List<Attendance> selectAttendanceByCri(AttendanceSearchCriteria cri) throws Exception;
 	public List<Attendance> selectAllAttendance() throws Exception;
 }
